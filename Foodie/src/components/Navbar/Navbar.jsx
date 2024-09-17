32c0c22ddc7713e19914b2aeb2cc34039c4713ae
Dilshad <diluk2005786@gmail.com>
2024-09-17 22:06:03 +0530

    First Commit

---
+++ Foodie/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+    <div className='shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200'>
+        <div className='container'>
+            <div className="flex justify-center items-center">
+                <div>
+                    <a href="#"></a>
+                </div>
+                <div></div>
+            </div>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Navbar
